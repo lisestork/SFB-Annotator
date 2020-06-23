5380b4c801efbd4096a44510e33987b89fc6ea58
arnikz <a.kuzniar@esciencecenter.nl>
2020-06-23 17:23:20 +0200

    Add func: failed()

--- run.sh
+++ run.sh
@@ -19,20 +19,26 @@ URL0="${BASE_URL}/semanticAnnotator/files/MMNAT01_AF_NNM001001033_001.jpg"
 URL1="${BASE_URL}/semanticAnnotator/writeAnnotationsToRDF"
 URL2="${BASE_URL}/rdf4j-server/repositories/${REPO_ID}"
 
+
+failed () {
+    echo "FAILED"
+    exit 1
+}
+
 # check if the web app servers the image file(s)
 echo -ne "Image file(s) served\t... "
 [[ $(curl -s -u "$CRE" -w "%{http_code}" "$URL0" -o test.jpg) -eq 200 ]] \
-    && echo "OK" || (echo "FAILED" && exit 1)
+    && echo "OK" || failed
 
 # validate JSON input
 echo -ne "Validate $JSON_FILE\t... "
 [[ $(python -m json.tool < "$JSON_FILE") ]] \
-    && echo "OK" || (echo "FAILED" && exit 1)
+    && echo "OK" || failed
 
 # create triples
 echo -ne "Create triples\t... "
 [[ $(curl -s -u "$CRE" -w "%{http_code}" -H "Content-Type: application/json" -d @"$JSON_FILE" "$URL1") -eq 200 ]] \
-    && echo "OK" || (echo "FAILED" && exit 1)
+    && echo "OK" || failed
 
 # count triples
 echo -ne "Count triples\t... "
@@ -43,14 +49,14 @@ echo "N=$N"
 # dump triples in RDF/Turtle
 echo -ne "Dump triples\t... "
 [[ $(curl -s -u "$CRE" -w "%{http_code}" -H 'Accept: text/turtle' "${URL2}/statements" -o "$RDF_FILE") -eq 200 ]] \
-    && echo "OK" || (echo "FAILED" && exit 1)
+    && echo "OK" || failed
 cat "$RDF_FILE"
 echo
 
 # delete triples
 echo -ne "Delete triples\t... "
 [[ $(curl -s -u "$CRE" -w "%{http_code}" -X DELETE "${URL2}/statements") -eq 204 ]] \
-    && echo "OK" || (echo "FAILED" && exit 1)
+    && echo "OK" || failed
 
 # count triples
 echo -ne "Count triples\t... "
