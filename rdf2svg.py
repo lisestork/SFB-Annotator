a27b0b9205a8829bc4e4cc303cc936a262158c5c
arnikz <a.kuzniar@esciencecenter.nl>
2021-02-05 13:55:59 +0100

    Update RDF graph layout #22.

--- rdf2svg.py
+++ rdf2svg.py
@@ -9,7 +9,7 @@ import rdflib
 from rdflib.namespace import (DC, DCTERMS, DOAP, FOAF, OWL, RDF, RDFS, SKOS,
                               VOID, XSD)
 
-parser = argp.ArgumentParser(description='Plot RDF graph in SVG.')                                            
+parser = argp.ArgumentParser(description='Plot RDF graph in SVG.')
 parser.add_argument('-i',
                     '--input',
                     help='Input in RDF/Turtle.',
@@ -59,13 +59,15 @@ G = nx.DiGraph()
 for (s, p, o) in g:
     G.add_node(s.n3(g.namespace_manager), group=s.n3(g.namespace_manager))
     G.add_node(o.n3(g.namespace_manager), group=o.n3(g.namespace_manager))
-    G.add_edge(s.n3(g.namespace_manager), o.n3(g.namespace_manager), group=p.n3(g.namespace_manager))
-
+    G.add_edge(s.n3(g.namespace_manager), o.n3(g.namespace_manager),
+               group=p.n3(g.namespace_manager))
 pos = nx.drawing.layout.spring_layout(G, iterations=1000)
 plt.figure()
-nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, 'group'), font_color='red', font_size=5)
-nx.draw(G, with_labels=True, pos=pos, node_size=12, node_color='lightgreen', edge_color='gray', font_size=5)
+nx.draw(G, pos, with_labels=True, node_shape='o', node_size=10,
+        node_color='lightblue', edge_color="gray", width=0.2, font_size=4)
+nx.draw_networkx_edge_labels(G, pos,
+                             edge_labels=nx.get_edge_attributes(G, 'group'),
+                             font_color='red', font_size=4)
 plt.title(title)
 plt.axis('off')
-plt.tight_layout()
 plt.savefig(outfile)
