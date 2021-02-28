f974bff96be740a49aacf3a77af0333976a7e5b9
arnikz <a.kuzniar@esciencecenter.nl>
2021-02-28 18:19:40 +0100

    Fix: delete annotation triples #71.

--- src/main/java/localdomain/localhost/servlets/removeAnnotationsFromRDF.java
+++ src/main/java/localdomain/localhost/servlets/removeAnnotationsFromRDF.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.query.QueryResults;
@@ -26,7 +27,7 @@ import org.eclipse.rdf4j.repository.http.HTTPRepository;
 public class removeAnnotationsFromRDF extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		// get httpRequest parameters
@@ -34,48 +35,36 @@ public class removeAnnotationsFromRDF extends HttpServlet {
 		String selector = request.getParameter("selector");
 
 		// connect to RDF server
-		String rdf4jServer = "http://localhost:8080/rdf4j-server/";
+		String host = "http://localhost:8080/";
 		String repositoryID = "mem-rdf";
-		Repository repo = new HTTPRepository(rdf4jServer, repositoryID);
-
-		String nhc = "http://makingsense.liacs.nl/rdf/nhc/";
+		Repository repo = new HTTPRepository(host + "rdf4j-server/", repositoryID);
 		ValueFactory f = repo.getValueFactory();
-		IRI selectorIRI = f.createIRI(nhc, selector);
-		IRI sourceIRI = f.createIRI(nhc, source);
-		IRI targetIRI = f.createIRI(nhc, source + selector);
 
-		String query1 = "SELECT DISTINCT ?x WHERE {?x <http://www.w3.org/ns/oa#hasTarget> <" + targetIRI.toString()
-				+ "> . }";
-		String annotationID = QueryTripleStoreString(query1, repo, "x");
-		IRI annotationIRI = f.createIRI(annotationID);
-
-		String query2 = "SELECT DISTINCT ?x WHERE { <" + annotationIRI.toString()
-				+ "> <http://www.w3.org/ns/oa#hasBody> ?x . }";
-		String body = QueryTripleStoreString(query2, repo, "x");
-		IRI bodyIRI = f.createIRI(body);
+		String queryStr = "PREFIX rdf: <" + RDF.NAMESPACE + "> \n";
+		queryStr += "PREFIX oa: <http://www.w3.org/ns/oa#> \n";
+		queryStr += "SELECT DISTINCT ?annot WHERE { \n";
+		queryStr += "	?annot oa:hasTarget/oa:hasSelector/rdf:value ?selector. \n";
+		queryStr += "	FILTER(?selector = \"" + selector + "\")}";
 
 		try (RepositoryConnection conn = repo.getConnection()) {
 			conn.begin();
-			conn.remove(annotationIRI, null, null);
-			conn.remove(targetIRI, null, null);
-			conn.remove(bodyIRI, null, null);
-			conn.remove(selectorIRI, null, null);
-			conn.remove(sourceIRI, null, null);
-			conn.commit();
-		}
-	}
-
-	public String QueryTripleStoreString(String query, Repository repo, String valueOf) {
-		String annotation;
-		try (RepositoryConnection conn = repo.getConnection()) {
-			TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, query);
-			List<BindingSet> resultList;
+			IRI annotationIRI = null;
+			TupleQuery tupleQuery = conn.prepareTupleQuery(queryStr);
 			try (TupleQueryResult result = tupleQuery.evaluate()) {
-				resultList = QueryResults.asList(result);
+				for (BindingSet solution : result) {
+					annotationIRI = (IRI) solution.getValue("annot");
+				}
+				conn.remove(annotationIRI, null, null);
+				conn.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn.close();
 			}
-			Value valueresult = resultList.get(0).getValue(valueOf);
-			annotation = valueresult.stringValue();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			repo.shutDown();
 		}
-		return annotation;
 	}
 }
