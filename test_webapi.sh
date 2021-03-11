e0d3715cdabd8c08eac86d458a3139b7e2b6eb85
arnikz <a.kuzniar@esciencecenter.nl>
2021-03-11 18:46:23 +0100

    Check for empty csv files.

--- test_webapi.sh
+++ test_webapi.sh
@@ -15,6 +15,7 @@ REPO_URL="http://localhost:8080/rdf4j-server/repositories/${REPO_ID}"
 SPARQL_ENDPOINT="http://sea:8080/rdf4j-server/repositories/${REPO_ID}"
 GRLC_URL="http://localhost:8088/api-local"
 GRLC_PATHS=(countAnnotations getAuthors getEvents getLocations getMeasurements getTaxa)
+CSV_DIR=data/csv
 
 # clean-up RDF store
 echo -ne "Delete triples\t... "
@@ -43,10 +44,13 @@ rm -fr queries
 git clone https://github.com/LINNAE-project/queries
 docker cp ./queries grlc:/home/grlc/
 
+# query Web API endpoints to fetch results in CSV
+mkdir -p $CSV_DIR
 for path in "${GRLC_PATHS[@]}"
 do
    local_url="$GRLC_URL/$path"
+   csv_file="${CSV_DIR}/${path}.csv"
    echo -ne "Test Web API endpoint '$local_url'\t... "
-   [[ $(curl -sL -H "accept: text/csv" -w "%{http_code}" "$local_url" --data-urlencode "endpoint=${SPARQL_ENDPOINT}" -o "data/csv/${path}.csv") -eq 200 ]] \
+   [[ $(curl -sL -H "accept: text/csv" -w "%{http_code}" "$local_url" --data-urlencode "endpoint=${SPARQL_ENDPOINT}" -o "$csv_file") -eq 200 && -s "$csv_file" ]] \
        && echo "OK" || failed
 done
