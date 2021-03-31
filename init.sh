41b42862cbfa70b8b8d159f0b35af4b00ec70ba3
arnikz <a.kuzniar@esciencecenter.nl>
2021-03-31 14:51:55 +0200

    Load example data #89.

--- init.sh
+++ init.sh
@@ -4,7 +4,10 @@ set -xe
 
 REPO_ID=mem-rdf
 CONF=conf/create_store.txt
+LOAD_PATH=/usr/local/tomcat/data/rdf/local/
 DATA_DIR=/var/rdf4j/server
 
 sleep 20
-sed "s/<REPO_ID>/${REPO_ID}/" $CONF | console.sh -e -f -d $DATA_DIR
+sed -i.org "s:<REPO_ID>:${REPO_ID}:" $CONF
+sed -i.org "s:<LOAD_PATH>:${LOAD_PATH}:" $CONF
+console.sh -e -f -d $DATA_DIR < $CONF
