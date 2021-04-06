5008d85149a527b3860c45e96be51bdc6f20a0b6
arnikz <a.kuzniar@esciencecenter.nl>
2021-04-06 15:44:40 +0200

    Fetch annotations in JSON #90 - GET /annotation request

--- run.sh
+++ run.sh
@@ -38,31 +38,35 @@ echo -ne "Validate $JSON_FILE\t... "
 [[ $(python -m json.tool < "$JSON_FILE") ]] \
     && echo "OK" || failed
 
-# delete triples
-echo -ne "Delete triples\t... "
+# delete RDF triples
+echo -ne "Delete RDF triples\t... "
 [[ $(curl -s -u "$CRE" -w "%{http_code}" -X DELETE "${REPO_URL}/statements") -eq 204 ]] \
     && echo "OK" || failed
 
-# count triples
-echo -ne "Count triples\t... "
+# count RDF triples
+echo -ne "Count RDF triples\t... "
 N=$(curl -s -u "$CRE" "{$REPO_URL}/size")
 echo "N=$N"
 [[ $N != 0 ]] && exit 1
 
-# create triples
-echo -ne "Create triples\t... "
+# create RDF triples from JSON input
+echo -ne "Create RDF triples\t... "
 [[ $(curl -s -u "$CRE" -w "%{http_code}" -H "Content-Type: application/json" -d @"$JSON_FILE" "$SEA_URL") -eq 201 ]] \
     && echo "OK" || failed
 
-# dump triples
-echo -ne "Dump triples\t... "
+# dump RDF triples
+echo -ne "Dump RDF triples\t... "
 [[ $(curl -s -u "$CRE" -w "%{http_code}" -H "Accept: ${MIME[$FILEXT]}" "${REPO_URL}/statements" -o "$RDF_FILE") -eq 200 ]] \
     && echo "OK" || failed
 cat "$RDF_FILE"
 
-# count triples
-echo -ne "Count triples\t... "
+# count RDF triples
+echo -ne "Count RDF triples\t... "
 N=$(curl -s -u "$CRE" "${REPO_URL}/size")
 echo "N=$N"
 [[ $N == 0 ]] && exit 1
+
+# fetch annotations in JSON
+echo -e "Get annotations in JSON\t... "
+curl -s -u "$CRE" -H "Content-Type: application/json" "$SEA_URL" | python -m json.tool
 echo "Done."
